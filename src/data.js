c516461a5c718918be213c872e430b92f9156396
flo_h <flo.1244@gmail.com>
2021-12-07 19:30:35 -0500

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const names = [
+  {person: "Ann", id:"001"},
+  {person: "Florence", id:"002"},
+  {person: "Johnny", id:"003"},
+  {person: "Carla", id:"004"},
+  {person: "Deigo", id:"005"},
+  {person: "Michelle", id:"006"}
+]
